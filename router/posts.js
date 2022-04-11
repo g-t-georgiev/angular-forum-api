5ec2ca8dbc2be0de3c97bae8bafdd3febcd2652b
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-11 20:44:57 +0300

    Added create, edit, delete post routes and create and delete likes routes.

--- router/posts.js
+++ router/posts.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../utils');
-const { postController } = require('../controllers');
+const { postController, likeController } = require('../controllers');
 
 // middleware that is specific to this router
 
 router.get('/', postController.getLatestsPosts);
+router.post('/', auth(), postController.createPost);
+router.put('/:postId', auth(), postController.editPost);
+router.delete('/:postId', auth(), postController.deletePost);
+
+router.post('/:postId/like', auth(), likeController.like);
+router.delete('/:postId/like', auth(), likeController.unlike);
 
 module.exports = router
