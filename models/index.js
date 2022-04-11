6564b7542a3f64d0672034576f81134963c15575
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-11 13:19:17 +0300

    Added 'likes' and 'subscriptions' models to models' common export file.

--- models/index.js
+++ models/index.js
@@ -2,10 +2,14 @@ const userModel = require('./userModel');
 const tokenBlacklistModel = require('./tokenBlacklistModel');
 const themeModel = require('./themeModel');
 const postModel = require('./postModel');
+const likeModel = require('./likeModel');
+const subscriptionModel = require('./subscriptionModel');
 
 module.exports = {
     userModel,
     tokenBlacklistModel,
     themeModel,
     postModel,
+    likeModel,
+    subscriptionModel
 }
