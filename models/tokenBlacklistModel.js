3e8c6a0c5ac31c1a5d395564507c183be1e781a7
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-11 18:59:09 +0300

    Set expiration time of black listed tokens database entries.

--- models/tokenBlacklistModel.js
+++ models/tokenBlacklistModel.js
@@ -5,6 +5,10 @@ const tokenBlacklistSchema = new Schema({
     token: String,
 }, { timestamps: true });
 
+// Set expiration time of a TokenBlackList document after 30 days of its creation date (time represented in seconds: 30 * 24 * 3600)
+// Set expiration test time of 60 seconds.
+tokenBlacklistSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 });
+
 const TokenBlacklist = model('TokenBlacklist', tokenBlacklistSchema);
 
 module.exports = TokenBlacklist;
