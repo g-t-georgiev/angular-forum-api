dc8862f26ba46c0306f40ee5d71f7f6bc5ad7819
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-11 20:44:15 +0300

    Removed 'likes' route and router reference as it is deleted.

--- router/index.js
+++ router/index.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
+
 const users = require('./users');
 const themes = require('./themes');
 const posts = require('./posts');
-const likes = require('./likes');
-const test = require('./test');
-const { authController } = require('../controllers');
 
+const { authController } = require('../controllers');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
@@ -13,7 +12,5 @@ router.post('/logout', authController.logout);
 router.use('/users', users);
 router.use('/themes', themes);
 router.use('/posts', posts);
-router.use('/likes', likes);
-router.use('/test', test);
 
 module.exports = router;
