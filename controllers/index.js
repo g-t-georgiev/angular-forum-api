effc0b2cd5b1ffa356f0a8b5d094fd411d2c564c
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-11 20:43:42 +0300

    Added 'users', 'likes' and 'subscriptions' controllers to common export.

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
 const authController = require('./auth');
 const themeController = require('./themeController');
 const postController = require('./postController');
+const userController = require('./userController');
+const likeController = require('./likeController');
+const subscriptionController = require('./subscriptionController');
 
 module.exports = {
     authController,
     themeController,
     postController,
+    userController,
+    likeController,
+    subscriptionController
 }
