6e97f742eecef88b2a42cc99f5c210b020c0f061
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-05-01 20:16:17 +0300

    Updated paths with new shortened file names.

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
-const userModel = require('./userModel');
-const tokenBlacklistModel = require('./tokenBlacklistModel');
-const themeModel = require('./themeModel');
-const postModel = require('./postModel');
-const likeModel = require('./likeModel');
-const subscriptionModel = require('./subscriptionModel');
+const userModel = require('./user');
+const tokenBlacklistModel = require('./tokenBlacklist');
+const themeModel = require('./theme');
+const postModel = require('./post');
+const likeModel = require('./like');
+const subscriptionModel = require('./subscription');
 
 module.exports = {
     userModel,
