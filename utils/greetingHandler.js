6177eb0d0a2fcdbf105e4ba9b25adf14cb0558bf
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-05-01 16:48:19 +0300

    Created greeting message handler.

---
+++ utils/greetingHandler.js
@@ -0,0 +1,10 @@
+function greet(username = 'stranger') {
+    const currentTime = new Date().getHours();
+    return currentTime <= 12 
+        ? `Good morning, ${username}!` 
+        : currentTime <= 18
+        ? `Good afternoon, ${username}!`
+        : `Good evening, ${username}!`;
+}
+
+module.exports = greet;
