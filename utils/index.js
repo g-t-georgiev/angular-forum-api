315fefce95b433aaed2ef22b942634f5a17f1b9c
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-05-03 19:11:42 +0300

    Removed error and auth middleware from utilities directory.

--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,7 @@
 const jwt = require('./jwt');
-const auth = require('./auth');
-const errorHandler = require('./errHandler');
 const greet = require('./greetingHandler');
 
 module.exports = {
     jwt,
-    auth,
-    errorHandler,
     greet
 }
