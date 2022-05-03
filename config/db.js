eb1e7465b01414666534bf3c1d3b80df65f3ce6a
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-05-03 11:47:51 +0300

    Configured line indent of 4 spaces.

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const config = require('./config');
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  return mongoose.connect(config.dbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  });
+	return mongoose.connect(config.dbURL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	});
 };
